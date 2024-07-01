58eb5a7befe7fab719ac07a85008eb5a46185a8f
권규리 <rudrb3770@naver.com>
2024-07-01 12:48:40 +0900

    start

---
+++ src/main/java/com/example/jmt/JmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jmt/JmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
