990f4469c82079078958fc95c6c759329aabf5b5
youngjun kim <youngjun4327@gmail.com>
2024-07-07 19:06:22 +0900

    aws IAM 접근,비밀키 값 수정

--- src/main/java/com/example/jmt/config/AwsS3Config.java
+++ src/main/java/com/example/jmt/config/AwsS3Config.java
@@ -6,15 +6,22 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class AwsS3Config {
 
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
     @Bean
     public AmazonS3 s3client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials("yourAccessKey", "yourSecretKey");
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
-                .withRegion("us-west-2")  // 버킷이 위치한 리전을 여기에 명시
+                .withRegion("ap-northeast-2")  // 버킷이 위치한 리전을 여기에 명시
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
